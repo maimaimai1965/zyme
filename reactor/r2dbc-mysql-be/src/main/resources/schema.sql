d85dd557c1382eb8b5780698a4c6f50afe511aae
Andrey Miroshnik <A.Miroshnik@telesens.ua>
2024-10-16 15:24:52 +0300

    splitting the r2dbc-mysql module into 2 modules - r2dbc-mysq-app and r2dbc-mysq-be. Adapting tests to the new structure.

---
+++ reactor/r2dbc-mysql-be/src/main/resources/schema.sql
@@ -0,0 +1,54 @@
+
+CREATE TABLE IF NOT EXISTS member (
+    member_id int not null auto_increment,
+    name varchar(64) not null,
+    primary key (member_id)
+);
+
+CREATE TABLE IF NOT EXISTS balance (
+                                       member_id int NOT NULL,
+                                       amount bigint NOT NULL,
+                                       created_date datetime NOT NULL,
+                                       last_modified_date datetime NOT NULL,
+                                       primary key (member_id)
+);
+alter table BALANCE add constraint FK_BALANCE__MEMBER_ID foreign key (member_id)
+    references MEMBER (member_id) on delete restrict on update restrict;
+
+
+CREATE TABLE IF NOT EXISTS transfer (
+    transfer_id bigint not null auto_increment,
+    from_member_id int,
+    to_member_id int NOT NULL,
+    amount bigint NOT NULL,
+    created_date datetime NOT NULL,
+    primary key (transfer_id)
+);
+alter table TRANSFER add constraint FK_TRANSFER__TO_MEMBER_ID foreign key (to_member_id)
+    references MEMBER (member_id) on delete restrict on update restrict;
+alter table TRANSFER add constraint FK_TRANSFER__FROM_MEMBER_ID foreign key (from_member_id)
+    references MEMBER (member_id) on delete restrict on update restrict;
+
+
+# /*==============================================================*/
+# /* Table: CN_PROVIDER                                           */
+# /*==============================================================*/
+# create table CN_PROVIDER
+# (
+#     PROVIDER_ID          mediumint not null comment 'Unique Contenet Privider ID (Code)',
+#     PROVIDER_NAME        varchar(32) not null,
+#     PROVIDER_TYPE        char(1) not null default 'N',
+#     STATE                char(1) not null,
+#     CREATED_DT           datetime not null,
+#     END_DT               datetime,
+#     primary key (PROVIDER_ID)
+# );
+#
+# alter table CN_PROVIDER
+#     add constraint CKC_PROVIDER_TYPE_CN_PROVIDER check (PROVIDER_TYPE in ('B','N','Y','E','O'));
+#
+# alter table CN_PROVIDER
+#     add constraint CKC_STATE_CN_PROVIDER check (STATE in ('E','D'));
+#
+# alter table CN_PROVIDER
+#     add constraint CKC_PROVIDER_NO_CN_PROVIDER check (PROVIDER_ID >= 100000 and PROVIDER_ID <= 999999);
---
+++ reactor/r2dbc-mysql-be/src/test/java/ua/mai/zyme/r2dbcmysql/controller/MemberControllerTest.java
@@ -0,0 +1,437 @@
+package ua.mai.zyme.r2dbcmysql.controller;
+
+import io.r2dbc.spi.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import ua.mai.zyme.r2dbcmysql.R2dbcMysqlApplicationTests;
+import ua.mai.zyme.r2dbcmysql.config.AppTestConfig;
+import reactor.core.publisher.Mono;
+import ua.mai.zyme.r2dbcmysql.entity.Member;
+import ua.mai.zyme.r2dbcmysql.exception.FaultInfo;
+import ua.mai.zyme.r2dbcmysql.repository.MemberRepository;
+import ua.mai.zyme.r2dbcmysql.util.TestUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // Запуск всего контекста на
+@ContextConfiguration(classes = {R2dbcMysqlApplicationTests.class})
+@EnableR2dbcRepositories(basePackages = {"ua.mai.zyme.r2dbcmysql.repository"})
+@ComponentScan(basePackages = {
+        "ua.mai.zyme.r2dbcmysql.service",
+        "ua.mai.zyme.r2dbcmysql.controller",
+        "ua.mai.zyme.r2dbcmysql.exception",
+})
+@Import(AppTestConfig.class)
+@EnableAutoConfiguration
+@AutoConfigureWebTestClient
+@Slf4j
+@ActiveProfiles(profiles = "test")
+class MemberControllerTest {
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private TestUtil tu;
+
+    @BeforeEach
+    public void setup() {
+        tu = new TestUtil();
+        tu.setConnectionFactory(connectionFactory);
+        tu.setMemberRepository(memberRepository);
+    }
+
+    @AfterEach
+    public void cleanup() {
+        // Teardown
+        tu.deleteMembersTestData();
+    }
+
+
+    // ------------------------------------ insertMember(member) <- /api/members ---------------------------------------
+
+    @Test
+    public void insertMember() {
+        // Setup
+        Member memberIn = TestUtil.newMember("mikeTest");
+
+        // Execution
+        List<Member> list =  webTestClient
+                .post()
+                .uri("/api/members")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(memberIn), Member.class)
+                .exchange()
+        // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream().toList();
+
+        assertTrue(list.size() == 1);
+        Member memberOut = list.get(0);
+        Member member_Db = tu.findMemberByMemberId(memberOut.getMemberId());
+        assertEquals(memberOut, member_Db);
+    }
+
+
+    // ------------------------------------ updateMember(member) <- /api/members ---------------------------------------
+
+    @Test
+    public void updateMember() {
+        // Setup
+        Member memberIn = tu.insertMember("ivanTest");
+        memberIn.setName("igorTEST");
+        // Execution
+        List<Member> listResult =  webTestClient
+                .put()
+                .uri("/api/members")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(memberIn), Member.class)
+                .exchange()
+        // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream().toList();
+
+        assertTrue(listResult.size() == 1);
+        Member memberOut = listResult.get(0);
+        assertEquals(memberIn, memberOut);
+        Member member_Db = tu.findMemberByMemberId(memberOut.getMemberId());
+        assertEquals(memberIn, member_Db);
+    }
+
+
+    // ------------------------------------ deleteMemberById(id) <- /api/members/{id} ----------------------------------
+
+    @Test
+    public void deleteMemberById() {
+        // Setup
+        Member memberIn = tu.insertMember("ivanTest");
+
+        // Execution
+        List<Member> listResult =  webTestClient
+                .delete()
+                .uri("/api/members/" + memberIn.getMemberId())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream().toList();
+
+        assertTrue(listResult.size() == 0);
+        assertNull(tu.findMemberByMemberId(memberIn.getMemberId()));
+    }
+
+    @Test
+    public void deleteMemberById_WhenMemberNotExists() {
+        // Execution
+        List<Member> listResult =  webTestClient
+                .delete()
+                .uri("/api/members/" + -1)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream().toList();
+
+        assertTrue(listResult.size() == 0);
+    }
+
+
+    // ------------------------------------ findAll() <- /api/members --------------------------------------------------
+
+    @Test
+    public void findAll() {
+        // Setup
+        List<Member> listIn = new ArrayList<>(List.of(
+                tu.insertMember("aniTest"),
+                tu.insertMember("budiTest"),
+                tu.insertMember("cepTest"),
+                tu.insertMember("dodTest")
+        ));
+
+        // Execution
+        List<Member> listResult = webTestClient
+                .get()
+                .uri("/api/members")   // Вычитываем все строки из таблицы (не только тестируемые)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream()
+                .filter(member -> TestUtil.isMemberForTest(member))  // Проверяем только на тестируемых данных
+                .toList();
+
+        Assertions.assertThat(listResult)
+                  .containsExactlyInAnyOrderElementsOf(listIn);
+    }
+
+
+    // ------------------------------------ findMemberByMemberId(id) <- /api/members/{id} ------------------------------
+
+    @Test
+    public void findMemberByMemberId() {
+        // Setup
+        Member memberIn = tu.insertMember("annaTest");
+        Integer id = memberIn.getMemberId();
+
+        // Execution
+        List<Member> listResult =  webTestClient
+                .get()
+                .uri("/api/members/" + id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream().toList();
+
+        assertTrue(listResult.size() == 1);
+        Member memberOut = listResult.get(0);
+        assertEquals(memberIn, memberOut);
+    }
+
+    @Test
+    public void findMemberByMemberId_ERR002_MemberNotFound() {
+        // Setup
+        Integer badMemberId = 1;
+
+        // Execution
+        webTestClient
+                .get()
+                .uri("/api/members/" + badMemberId)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().value(status -> assertEquals("404", status.toString()))
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertTrue(body.contains(FaultInfo.NOT_FOUND_CODE));
+                });
+    }
+
+    @Test
+    public void findMemberByMemberId_ERR001_BadMemberId() {
+        // Setup
+        String badMemberId = "12BAD";
+
+        // Execution
+        webTestClient
+                .get()
+                .uri("/api/members/" + badMemberId)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().value(status -> assertEquals("400", status.toString()))
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertTrue(body.contains(FaultInfo.UNEXPECTED_ERROR_CODE));
+                });
+    }
+
+    @Test
+    public void findMemberByMemberId_ERR001_MethodNotAllowed() {
+        // Setup
+
+        // Execution
+        webTestClient
+                .put()
+                .uri("/api/members/" + 1)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().value(status -> assertEquals("405", status.toString()))
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertTrue(body.contains(FaultInfo.UNEXPECTED_ERROR_CODE));
+                });
+    }
+
+    @Test
+    public void findMemberByMemberId_ERR001_NoStaticResource() {
+        // Setup
+
+        // Execution
+        webTestClient
+                .put()
+                .uri("/api/memb")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().value(status -> assertEquals("404", status.toString()))
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertTrue(body.contains(FaultInfo.UNEXPECTED_ERROR_CODE));
+                });
+    }
+
+
+    // ------------------------------------ findMemberByName(name) <- /api/members?name= -------------------------------
+
+    @Test
+    public void findMemberByName() {
+        // Setup
+        Member memberIn = tu.insertMember("annaTest");
+        Integer id = memberIn.getMemberId();
+
+        // Execution
+        List<Member> listResult =  webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/members")
+                        .queryParam("name", "annaTest")
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .attribute("name", "annaTest")
+                .exchange()
+        // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream().toList();
+
+        assertTrue(listResult.size() == 1);
+        Member memberOut = listResult.get(0);
+        assertEquals(memberIn, memberOut);
+    }
+
+    @Test
+    public void findMemberByName_ERR002_MemberNotFound() {
+        // Setup
+
+        // Execution
+        webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/members")
+                        .queryParam("name", "$$$$$")
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().value(status -> assertEquals("404", status.toString()))
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertTrue(body.contains(FaultInfo.NOT_FOUND_CODE));
+                });
+    }
+
+
+    // ------------------------------------ findMembersByNameLike(nameLike) <- /api/members?nameLike= ------------------
+
+    @Test
+    public void findMembersByNameLike() {
+        // Setup
+        List<Member> listIn = new ArrayList<>(List.of(
+                tu.insertMember("vinsenTest"),
+                tu.insertMember("bearnTest"),
+                tu.insertMember("tomTest"),
+                tu.insertMember("redlTest")
+        ));
+        List<Member> listForCheck = listIn.stream().filter(member -> member.getName().contains("nTest")).toList();
+
+        // Execution
+        List<Member> listResult = webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/members")
+                        .queryParam("nameLike", "nTest")
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream().toList();
+
+        Assertions.assertThat(listForCheck)
+                  .containsExactlyInAnyOrderElementsOf(listResult);
+    }
+
+    @Test
+    public void findMembersByNameLike_WhenEmptyList() {
+        // Setup
+        List<Member> listIn = new ArrayList<>(List.of(
+                tu.insertMember("vinsenTest"),
+                tu.insertMember("bearnTest"),
+                tu.insertMember("tomTest"),
+                tu.insertMember("redlTest")
+        ));
+
+        // Execution
+        List<Member> listResult = webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/members")
+                        .queryParam("nameLike", "&&&&")
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().isOk()  // 200
+                .returnResult(Member.class)
+                .getResponseBody()
+                .toStream().toList();
+
+        Assertions.assertThat(listResult).isEmpty();
+    }
+
+    @Test
+    public void findMembersByNameLike_ERR001_BadRequest_400() {
+        // Setup
+
+        // Execution
+        webTestClient
+                .post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/members")
+                        .queryParam("nameLike", "&&&&")
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+        // Assertion
+                .expectStatus().value(status -> assertEquals("400", status.toString()))
+                .expectBody()
+                .consumeWith(response -> {
+                    String body = new String(response.getResponseBody());
+                    assertTrue(body.contains(FaultInfo.UNEXPECTED_ERROR_CODE));
+                });
+    }
+
+}
